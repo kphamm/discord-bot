6c5f591c8519731dd1252d9659352cb17c18838f
Kevin Pham <k_pham@ucsb.edu>
2022-01-23 02:49:41 -0800

    convert

---
+++ commands/convert.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const rp = require('request-promise');
+const coin = process.env.coin;
+let firstCrypto, secondCrypto;
+
+
+const data = new SlashCommandBuilder()
+	.setName('convert')
+	.setDescription('Converts a crypto to another')
+    .addStringOption(option =>
+        option.setName('input')
+			.setDescription('<number> <crypto you want to convert> <crypto you want to convert to>')
+			.setRequired(true));
+
+module.exports = {
+	data,
+	async execute(interaction) {
+		const input = interaction.options.getString('input');
+        const inputArr = input.split(' ');
+        const num = inputArr[0];
+        firstCrypto = inputArr[1].toUpperCase();
+        secondCrypto = inputArr[2].toUpperCase();
+        let firstCryptoPrice, secondCryptoPrice;
+        const firstCryptoCall = {
+            method: 'GET',
+            uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest',
+            qs: {
+              'symbol': `${firstCrypto}`
+            },
+            headers: {
+              'X-CMC_PRO_API_KEY': `${coin}`
+            },
+            json: true,
+            gzip: true
+          };
+        await rp(firstCryptoCall).then(response => {
+            firstCryptoPrice = response.data[`${firstCrypto}`].quote.USD.price;
+          }).catch((err) => {
+            console.log('API call error:', err.message);
+          });
+
+        const secondCryptoCall = {
+            method: 'GET',
+            uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest',
+            qs: {
+                'symbol': `${secondCrypto}`
+            },
+            headers: {
+                'X-CMC_PRO_API_KEY': `${coin}`
+            },
+            json: true,
+            gzip: true
+        };
+        await rp(secondCryptoCall).then(response => {
+            secondCryptoPrice = response.data[`${secondCrypto}`].quote.USD.price;
+          }).catch((err) => {
+            console.log('API call error:', err.message);
+          });
+        
+          const answer = num * firstCryptoPrice/secondCryptoPrice
+		return interaction.reply({ content: `${num} ${firstCrypto} is equal to ${answer} ${secondCrypto}`, ephemeral: false });
+	},
+};
