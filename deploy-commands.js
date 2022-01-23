5f2ff6f81861546560c3d5241743b9567373a8e0
Kevin Pham <k_pham@ucsb.edu>
2022-01-23 00:56:57 -0800

    uses dotenv

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+require('dotenv').config();
+
+const token = process.env.token;
+const guildId = process.env.guildId;
+const clientId = process.env.clientId;
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
